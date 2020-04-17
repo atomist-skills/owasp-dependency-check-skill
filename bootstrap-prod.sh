63676f5e0ca0e05bf6c449b060c4191a93d29fb7
Jim Clark <slimslenderslacks@gmail.com>
2020-04-16 22:26:45 -0700

    initial report

---
+++ bootstrap-prod.sh
@@ -0,0 +1,1 @@
+cd /Users/slim/skills/package-cljs-skill && lein run -m atomist.functions.bootstrap $API_KEY_SLIMSLENDERSLACKS_PROD_GITHUB_AUTH prod $1 $2 $3
---
+++ bootstrap-staging.sh
@@ -0,0 +1,1 @@
+cd /Users/slim/skills/package-cljs-skill && lein run -m atomist.functions.bootstrap $API_KEY_SLIMSLENDERSLACKS_STAGING staging $1 $2 $3
